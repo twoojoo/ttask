a37a767d2c931cff37f3cd829fd75e97c0ddce98
Giovanni Campeol <giovanni@smartpricing.it>
2023-10-29 10:49:53 +0100

    added user input example

---
+++ examples/input/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	. "github.com/twoojoo/ttask/operator"
+	. "github.com/twoojoo/ttask/source"
+	. "github.com/twoojoo/ttask/task"
+)
+
+func main() {
+	ctx := context.Background()
+
+	T(
+		FromReadline("t1", "> type a phrase: "),
+		Print[string]("> you typed:"),
+	).Catch(func(m *Meta, e error) {
+		log.Fatal(e)
+	}).Run(ctx)
+
+	T(T(
+		FromReadChar("t2", "> now type a char: "),
+		Map(func(x rune) string { return string(x) })),
+		Print[string]("> you typed:"),
+	).Catch(func(m *Meta, e error) {
+		log.Fatal(e)
+	}).Run(ctx)
+
+}
