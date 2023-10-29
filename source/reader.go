0ba04a21306039915f4b1b481fd8004387fac8dd
Giovanni Campeol <giovanni@smartpricing.it>
2023-10-29 11:26:13 +0100

    added reader source

---
+++ source/reader.go
@@ -0,0 +1,30 @@
+package operator
+
+import (
+	"io"
+
+	"github.com/twoojoo/ttask/task"
+)
+
+func fromReader(r io.Reader, bufSize int) task.Operator[any, []byte] {
+	return func(m *task.Meta, x *task.Message[any], next *task.Step) {
+		buf := make([]byte, bufSize)
+
+		for {
+			n, err := r.Read(buf)
+
+			if err != io.EOF {
+				if err != nil {
+					m.Error(err)
+					return
+				}
+
+				m.ExecNext(task.NewMessage(buf[:n]), next)
+			}
+		}
+	}
+}
+
+func FromReader(taskId string, r io.Reader, bufSize int) *task.TTask[any, []byte] {
+	return task.T(task.Task[any](taskId), fromReader(r, bufSize))
+}
