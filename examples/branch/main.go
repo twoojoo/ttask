16e436f51b8710688ecbc6d1ca98df855754d983
Giovanni Campeol <giovanni@smartpricing.it>
2023-11-03 17:18:39 +0100

    alt syntax in branch example

--- examples/branch/main.go
+++ examples/branch/main.go
@@ -18,16 +18,27 @@ func main() {
 		log.Fatal(e)
 	}).Lock()
 
-	t := T(T(T(
-		Injectable[string]("t1"),
-		Print[string]("> first:")),
-		Branch[string](branch)),
-		Print[string]("> second:"),
-	).Catch(func(m *Inner, e error) {
-		log.Fatal(e)
-	}).Lock()
+	// ALTERNATIVE SINTAX
 
-	err := t.Inject(context.Background(), "message")
+	/**
+	 * pay attention:
+	 * all variables are pointers to the
+	 * same task instance!!
+	 * */
+
+	source := Injectable[string]("t1")
+
+	step1 := T(source, Print[string]("> first:"))
+
+	step2 := T(step1, Branch[string](branch))
+
+	task := T(step2, Print[string]("> second:"))
+
+	task.Lock().Catch(func(m *Inner, e error) {
+		log.Fatal(e)
+	})
+
+	err := task.Inject(context.Background(), "message")
 	if err != nil {
 		log.Fatal(err)
 	}
