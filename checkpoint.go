3f780fa97aa47144e190bc61598e988d8c6b3a46
Giovanni Campeol <giovanni@smartpricing.it>
2023-11-04 10:29:03 +0100

    better checkpoint description

--- checkpoint.go
+++ checkpoint.go
@@ -2,12 +2,13 @@ package ttask
 
 // Turns an opertator into a checkpoint.
 //
-// If the processing of a message is interrupted before reaching the next checkpoint or before the last
+// If the processing of a message is interrupted before reaching the next checkpoint (windows act as checkpoints) or before the last
 // task operation is exectuted, the next task execution will recover the checkpointed messages.
 //
 // The recovery procedure will start when a new message reach the checkpoint.
 //
-// DON'T USE if you have more than one replica of the same task and you're using a remote storage like Redis.
+// DON'T USE if you have more than one replica of the same task and you're using a remote storage like Redis,
+// unless you find a way to set a different task id for each replica and you're able to retain it on each restart.
 //
 // USELESS if your using the in-memory storage or if you're using a local storage you're running the task
 // in a volatile resource (e.g. K8s pods)
