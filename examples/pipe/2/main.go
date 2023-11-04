517e24e7f8692590e097151859e93dc5339e0486
Giovanni Campeol <giovanni@smartpricing.it>
2023-11-04 17:05:34 +0100

    more pipe examples

---
+++ examples/pipe/2/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	. "github.com/twoojoo/ttask"
+)
+
+func main() {
+	ctx := context.Background()
+
+	T(T(T(
+		FromStdin("t1"),
+		Tap(func(x string) {
+			log.Println("> received", x)
+		})),
+		Delay[string](time.Second)),
+		ToStdout(func(x string) string {
+			return x
+		}),
+	).Catch(func(i *Inner, e error) {
+		log.Fatal(e)
+	}).Run(ctx)
+}
