0aa97734817a8e6c11ed6203ad306d9049755172
Giovanni Campeol <giovanni@smartpricing.it>
2023-11-04 10:24:22 +0100

    explicit file example

--- examples/file/main.go
+++ examples/file/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"strings"
 
-	"github.com/twoojoo/ttask"
+	tt "github.com/twoojoo/ttask"
 )
 
 /**
@@ -17,18 +17,18 @@ func main() {
 	ctx := context.Background()
 
 	in := "./examples/file/in.txt"
-	out := "./examples/file/out.txt"
+	out := "./examples/file/outt.txt"
 
-	ttask.T(ttask.T(ttask.T(ttask.T(ttask.T(
-		ttask.FromFile("t1", in),
-		ttask.Print[string]("#1 - received:\t\t")),
-		ttask.Map(func(x string) string {
+	tt.T(tt.T(tt.T(tt.T(tt.T(
+		tt.FromFile("t1", in),
+		tt.Print[string]("#1 - received:\t\t")),
+		tt.Map(func(x string) string {
 			return strings.ToUpper(x)
 		})),
-		ttask.Print[string]("#2 - transformed:\t")),
-		ttask.ToFile(out, "|")),
-		ttask.Print[string]("#3 - written:\t\t"),
-	).Catch(func(i *ttask.Inner, e error) {
+		tt.Print[string]("#2 - transformed:\t")),
+		tt.ToFile(out, "|")),
+		tt.Print[string]("#3 - written:\t\t"),
+	).Catch(func(i *tt.Inner, e error) {
 		log.Fatal(e)
 	}).Run(ctx)
 }
