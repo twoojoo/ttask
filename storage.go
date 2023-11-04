52b9c305b31632e23678a7f5e7f67f1c84454955
Giovanni Campeol <giovanni@smartpricing.it>
2023-11-04 13:23:28 +0100

    comments on storage

--- storage.go
+++ storage.go
@@ -20,10 +20,12 @@ type windowMeta struct {
 }
 
 type Storage interface {
+	//checkpointing
 	storeCheckpoint(taskId string, msgId string, cpId string, msg []byte) error
 	clearCheckpoint(taskId string, msgId string, cpId string) error
 	getCheckpointMessages(taskId string, cpId string) ([][]byte, error)
 
+	//windowing
 	startNewEmptyWindow(cpId string, key string, start ...time.Time) (windowMeta, error)
 	startNewWindow(cpId string, key string, msg any, start ...time.Time) (windowMeta, error)
 	pushMessageToWindow(cpId string, key string, winId string, msg any) (int, error)
