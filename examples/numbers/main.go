b5553538df450492216f440eb356d22479630eca
Giovanni Campeol <giovanni@smartpricing.it>
2023-11-10 19:21:45 +0100

    better numbers example

--- examples/numbers/main.go
+++ examples/numbers/main.go
@@ -10,14 +10,16 @@ import (
 func main() {
 	ctx := context.Background()
 
+	numbers := [][]int{
+		{1, 2, 3, 4},
+		{0, 1, 2, 3, 4},
+		{15, 16, 17, 18, 19},
+		{5, 6, 7, 8, 9},
+		{10, 11, 12, 13, 14},
+	}
+
 	T(T(T(
-		FromItem("numbers-example", [][]int{
-			{0, 1, 2, 3, 4},
-			{0, 1, 2, 3, 4},
-			{15, 16, 17, 18, 19},
-			{5, 6, 7, 8, 9},
-			{10, 11, 12, 13, 14},
-		}),
+		FromItem("sum-example", numbers),
 		FlatArray[int]()),
 		Sum[int]()),
 		Print[int]("sum >"),
@@ -26,15 +28,22 @@ func main() {
 	}).Run(ctx)
 
 	T(T(T(
-		FromItem("numbers-example", [][]int{
-			{1, 2, 7, 23, 11, 3, 15, 4, 12, 5},
-			{15, 16, 17, 18, 19},
-			{5, 6, 7, 8, 9},
-			{10, 11, 12, 13, 14},
-		}),
+		FromItem("multiply-example", numbers),
 		FlatArray[int]()),
 		Multiply[int]()),
 		Print[int]("product >"),
+	).Catch(func(i *Inner, e error) {
+		log.Fatal(e)
+	}).Run(ctx)
+
+	T(T(T(T(
+		FromItem("average-example", numbers),
+		FlatArray[int]()),
+		MapArray(func(x int) float64 {
+			return float64(x)
+		})),
+		Average[float64]()),
+		Print[float64]("average >"),
 	).Catch(func(i *Inner, e error) {
 		log.Fatal(e)
 	}).Run(ctx)
